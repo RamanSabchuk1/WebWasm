e6bed97db06b10f6ed267cd65f32415fcf826d0e
Roman Sabchuk <raman.sabchuk@leverx.com>
2026-01-17 00:52:38 +0300

    Started adding

---
+++ WebWasm/Models/Suggestion.cs
@@ -0,0 +1,3 @@
+﻿namespace WebWasm.Models;
+
+public record Suggestion(Guid Id, Guid UserInfoId, string Name, Dictionary<string, string> Data, DateTime Created, DateTime? Applied);
---
+++ WebWasm/Pages/Supports.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebWasm.Models;
+using WebWasm.Services;
+
+namespace WebWasm.Pages;
+
+public partial class Supports(ApiClient apiClient) : ComponentBase
+{
+    private ICollection<Suggestion> _suggestions = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        _suggestions = await apiClient.Get<ICollection<Suggestion>>("Supports/suggestion/all");
+    }
+}
